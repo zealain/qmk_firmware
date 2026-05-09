4c48762e822c0695b790a66745e9660bbd3f9be7
zealain <zealained@gmail.com>
2026-05-09 21:54:28 +0200

    Add customizations

--- keyboards/keebio/bdn9/rev2/rev2.c
+++ keyboards/keebio/bdn9/rev2/rev2.c
@@ -5,26 +5,10 @@
 
 bool encoder_update_kb(uint8_t index, bool clockwise) {
     if (!encoder_update_user(index, clockwise)) { return false; }
-    if (index == 0) {
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
-    }
-    else if (index == 1) {
-        if (clockwise) {
-            tap_code(KC_DOWN);
-        } else {
-            tap_code(KC_UP);
-        }
-    }
-    else if (index == 2) {
-        if (clockwise) {
-            tap_code(KC_PGDN);
-        } else {
-            tap_code(KC_PGUP);
-        }
+    if (clockwise) {
+        tap_code(KC_VOLU);
+    } else {
+        tap_code(KC_VOLD);
     }
     return false;
 }
